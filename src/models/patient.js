d2ba169b9fa5e90fce248f69eae8d4724cc28795
minhlq2311 <minhlq2311@gmail.com>
2025-10-09 02:25:41 +0700

    Create register and login function

--- src/models/patient.js
+++ src/models/patient.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const generate = require("../helpers/generate");
 
 const patientSchema = new mongoose.Schema({
   name: { type: String, required: true },
@@ -6,6 +7,20 @@ const patientSchema = new mongoose.Schema({
   phone: { type: String, required: true, unique: true },
   email: { type: String, unique: true },
   address: { type: String },
+  password: { type: String, required: true },
+  tokenUser: {
+        type: String,
+        default: generate.generateRandomString(20)
+  },
+  status: {
+      type: String,
+      default: "active"
+    },
+    deleted: {
+      type: Boolean,
+      default: false
+    },
+    deletedAt: Date
 }, {
   timestamps: true
 });
