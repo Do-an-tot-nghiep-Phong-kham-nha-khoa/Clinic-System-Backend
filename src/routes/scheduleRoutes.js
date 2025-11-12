5700e50df0193eae12618d74631d967273dc025c
minhlq2311 <minhlq2311@gmail.com>
2025-11-13 05:07:52 +0700

    Show slot available for doctor by date

--- src/routes/scheduleRoutes.js
+++ src/routes/scheduleRoutes.js
@@ -4,8 +4,9 @@ const controller = require('../controllers/scheduleController');
 
 router.get('/specialty/:specialty_id/:date', controller.getAvailableTimeSlotsBySpecialty);
 
-router.get('/:doctor_id', controller.getDoctorScheduleByID);
 router.get('/:doctor_id/:date', controller.getDoctorScheduleByDate);
+router.get('/:doctor_id', controller.getDoctorScheduleByID);
+
 
 router.post('/', controller.createSchedule);
 
